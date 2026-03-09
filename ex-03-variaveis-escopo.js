e4e2b3801f0dbe854f8fd37032fea7fac1a2f2e5
kevenlwr <keven.santana@nave.org.br>
2026-03-09 13:53:33 -0300

    atividade

--- ex-03-variaveis-escopo.js
+++ ex-03-variaveis-escopo.js
@@ -150,31 +150,31 @@ MÍNIMO DE RESPOSTAS NECESSÁRIAS: 4
 */
 
 /* Questão 01: avdol enxerga jotaro?*/
-export const resposta01 = undefined
+export const resposta01 = true
 
 /* Questão 02: kira enxerga kakyoin?*/
-export const resposta02 = undefined
+export const resposta02 = false
 
 /* Questão 03: wham enxerga polnareff?*/
-export const resposta03 = undefined
+export const resposta03 = false
 
 /* Questão 04: emporio enxerga bruno?*/
-export const resposta04 = undefined
+export const resposta04 = true
 
 /* Questão 05: jolyne enxerga bruno?*/
-export const resposta05 = undefined
+export const resposta05 = true
 
 /* Questão 06: speedwagon enxerga koichi?*/
-export const resposta06 = undefined
+export const resposta06 = false
 
 /* Questão 07: kars enxerga joseph?*/
-export const resposta07 = undefined
+export const resposta07 = false
 
 /* Questão 08: hermes enxerga smokey?*/
-export const resposta08 = undefined
+export const resposta08 = false
 
 /* Questão 09: lisaLisa enxerga giorno?*/
-export const resposta09 = undefined
+export const resposta09 = false
 
 /* Questão 10: giorno enxerga mista?*/
-export const resposta10 = undefined
+export const resposta10 = true
